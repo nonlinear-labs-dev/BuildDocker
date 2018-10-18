c0cbedede28cfabf7d22da2155bdc0c0700f515c
schlenkibus <justus.scheil@parteimail.de>
2018-10-18 18:06:37 +0200

    added flag to exclude deploy of online help by default

--- deploy-build.sh
+++ deploy-build.sh
@@ -1,20 +1,40 @@
 #!/bin/sh
 
+copyonlinehelp="$3"
+
+if [ -z $copyonlinehelp ]
+then
+	copyonlinehelp="off"
+fi
+
 ip="192.168.8.2"
 version=`date +"%Y-%m-%d-%H-%M"`
 branch="$1"
+user="root"
 builPath="$2"
 targetdir=/nonlinear/playground-$branch-$version
 
 # copy all the stuff into a new directory
-scp -r $2 root@$ip:$targetdir
-
+if [ "$copyonlinehelp" = 'on' ]
+then
+	scp -r $2 $user@$ip:$targetdir
+elif [ "$copyonlinehelp" = 'off' ]
+then
+	rsync -av -e ssh --exclude='*/online-help/*' $2 $user@$ip:$targetdir
+fi
 #if it is a link:
-ssh root@$ip "rm /nonlinear/playground" > /dev/null 2>&1
+ssh $user@$ip "rm /nonlinear/playground" > /dev/null 2>&1
 
 #if it is a directory (fresh sd card):
-ssh root@$ip "mv /nonlinear/playground /nonlinear/playground-old" > /dev/null 2>&1
+ssh $user@$ip "mv /nonlinear/playground /nonlinear/playground-old" > /dev/null 2>&1
+
 
 # finalize
-ssh root@$ip "ln -s $targetdir /nonlinear/playground"
-ssh root@$ip "systemctl restart playground"
+ssh $user@$ip "ln -s $targetdir /nonlinear/playground"
+
+if [ "$copyonlinehelp" = 'off' ]
+then
+	ssh $user@$ip "echo 'Not Copied!' > /nonlinear/playground/NonMaps/war/online-help/index.html"
+fi
+
+ssh $user@$ip "systemctl restart playground"
